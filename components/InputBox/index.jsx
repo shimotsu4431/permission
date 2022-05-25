9154f8a983b3cfd999e8f27e43a5b1a8b1a877fd
shimotsu4431 <shimotsu13@gmail.com>
2022-05-25 23:20:06 +0900

    add logic

--- components/InputBox/index.jsx
+++ components/InputBox/index.jsx
@@ -14,16 +14,16 @@ export default function InputBox({ isFileType = false }) {
       onChange={e => setText(e.target.value)}
       maxLength={1}
       className={clsx([
-        text === ''
-          ? 'rounded-sm border-2 border-dashed border-indigo-100'
-          : 'rounded-sm border-2 border-indigo-600',
+        targetText.includes(text)
+          ? 'rounded-sm border-2 border-indigo-600'
+          : 'rounded-sm border-2 border-dashed border-indigo-100',
         'w-16',
         'h-16',
         'text-center',
         'text-3xl',
         'font-bold',
         'm-1',
-        'outline-none'
+        'outline-none',
       ])}
     ></input>
   )
