f98d0324a59210656a90ac870bf81732db6c8973
shimotsu4431 <shimotsu13@gmail.com>
2022-05-26 17:50:06 +0900

    fix getPermissionOctal

--- utils/permission.js
+++ utils/permission.js
@@ -21,7 +21,6 @@ export const getPermissionOctal = list => {
   const write_num = write === 'w' ? 1 : 0
   const execute_num = execute === 'x' ? 1 : 0
 
-  // const result = read_num * 100 + write_num * 10 + execute_num
   const result = `${read_num}${write_num}${execute_num}`
 
   return result
