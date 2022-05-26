2c6a34c4f3270b51328c09f3967719afb568ca17
shimotsu4431 <shimotsu13@gmail.com>
2022-05-26 17:57:59 +0900

    impl initArray

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,50 +4,16 @@ import { atom, useRecoilValue } from 'recoil'
 import InputBox from '../components/InputBox'
 import { getPermissionBinary, getPermissionOctal } from '../utils/permission'
 
+const initArray = [...Array(10)].map((_, i) => {
+  return {
+    index: i,
+    value: '-'
+  }
+});
+
 export const textState = atom({
   key: 'textState',
-  default: [
-    {
-      index: 0,
-      value: '-',
-    },
-    {
-      index: 1,
-      value: '-',
-    },
-    {
-      index: 2,
-      value: '-',
-    },
-    {
-      index: 3,
-      value: '-',
-    },
-    {
-      index: 4,
-      value: '-',
-    },
-    {
-      index: 5,
-      value: '-',
-    },
-    {
-      index: 6,
-      value: '-',
-    },
-    {
-      index: 7,
-      value: '-',
-    },
-    {
-      index: 8,
-      value: '-',
-    },
-    {
-      index: 9,
-      value: '-',
-    },
-  ],
+  default: initArray,
 })
 
 export default function Home() {
