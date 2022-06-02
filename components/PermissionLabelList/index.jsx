621cf8c29840fcfb858e40644ed5167e92be08b7
shimotsu4431 <shimotsu13@gmail.com>
2022-06-02 23:52:31 +0900

    fix PermissionLabelList

--- components/PermissionLabelList/index.jsx
+++ components/PermissionLabelList/index.jsx
@@ -1,12 +1,16 @@
 import PermissionLabel from '../PermissionLabel'
 
+const list = ["読み", "書き", "実行"]
+
 export default function PermissionLabelList({ startIndex }) {
   return (
     <div className="mt-4">
       <ul className="flex">
-        <PermissionLabel index={startIndex} label={'読み'} />
-        <PermissionLabel index={startIndex + 1} label={'書き'} />
-        <PermissionLabel index={startIndex + 2} label={'実行'} />
+        {list.map((item, i) => {
+          return (
+            <PermissionLabel key={i} index={startIndex + i} label={item} />
+          )
+        })}
       </ul>
     </div>
   )
